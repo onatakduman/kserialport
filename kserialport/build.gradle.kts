758e01ccbfefeb4771af587d66108622593473bb
Onat Akduman <onatakduman@gmail.com>
2026-01-07 15:26:10 +0300

    Enable dynamic versioning and add release automation workflow

--- kserialport/build.gradle.kts
+++ kserialport/build.gradle.kts
@@ -57,7 +57,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.onatakduman"
             artifactId = "kserialport"
-            version = "1.0.6"
+            version = project.version.toString()
 
             afterEvaluate {
                 from(components["release"])
